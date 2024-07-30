dadbb8c2b39eca3ed348f7f5bbae02d87360b2be
lara-sorrenson <l2.sorrenson@connect.qut.edu.au>
2024-07-30 10:16:30 +1000

    commit msg

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -1,6 +1,18 @@
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
 public class ToDoItem {
+    @Getter
     private String description;
     private boolean isDone;
+
+    public boolean isDone() {
+        return false;
+    }
 
     // TODO Now:
     // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
